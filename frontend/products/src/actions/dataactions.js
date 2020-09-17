a6aa2dc961deb6356373b99d9f3cd9b386155448
Lopes-7 <gui-lopes7@live.com>
2020-09-17 15:29:56 -0300

    [FRONTEND] Add data actions

---
+++ frontend/products/src/actions/dataactions.js
@@ -0,0 +1,18 @@
+/**
+ * CODE
+ */
+
+// action creator for update action
+function update() {
+    return {type: 'UPDATE'};
+}
+
+// action creator for updated action
+function updated() {
+    return {type: 'UPDATED'};
+}
+
+/**
+ * EXPORTS
+ */
+export {update, updated};
